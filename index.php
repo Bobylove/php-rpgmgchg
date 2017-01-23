0ed0cf3b3cc1cd3dbcd34c242eb0f0f1a2871c4e
Fred <delon.frederic.pro.job@gmail.com>
2017-01-23 11:51:19 +0100

    add semantic ui tables design

--- index.php
+++ index.php
@@ -3,48 +3,50 @@
 <head>
 	<meta charset="UTF-8">
 	<title>tab</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.7/semantic.min.css">
 </head>
 <body>
 	<h1>
-		<table>
+		<table class="ui table">
 			<tr>
 				<th>ID</th>
 				<th>Email</th>
 				<th>Date</th>
 			</tr>
-			<?php include "userTable.php" ;?>
+			<?php include "views/userTable.php" ;?>
 		</table></h1>
 		<br>
 		<br>
 		<br>
 
-<table>
-	<tr>
-		<th>ID</th>
-		<th>Nom</th>
-		<th>Prix</th>
-		<th>Marque</th>
-		<th>Producteur</th>
-		<th>Recolte</th>
-	</tr>
+		<table class="ui table">
+			<tr>
+				<th>ID</th>
+				<th>Nom</th>
+				<th>Prix</th>
+				<th>Marque</th>
+				<th>Producteur</th>
+				<th>Recolte</th>
+			</tr>
 
-	<?php
+			<?php
 
-	$prod = require('productTable.php');
+			$prod = require('views/productTable.php');
 
-	?>
-	<?php foreach ($prod as $key => $value) { ?>
-	<tr><?php echo  (method_exists($value, 'getId')) ? "<td>". $value->getId() : null ;?>
-		<?php echo  (method_exists($value, 'getName')) ? "<td>". $value->getName()  : null ;?>
-		<?php echo  (method_exists($value, 'getPrix')) ? "<td>".  $value->getPrix()  : null ;?>
-		<?php echo  (method_exists($value, 'getBrand')) ? "<td>". $value->getBrand()  : null ;?>
-		<?php echo  (method_exists($value, 'getProductorName')) ? "<td>". $value->getProductorName() :null;?>
-		<?php echo  (method_exists($value, 'getHarverstedAt')) ? "<td>". $value->getHarverstedAt()  : null;?></tr>
-		<?php } ?>
-</table>
-<br>
-<br>
-<br>
+			?>
+			<?php foreach ($prod as $key => $value) { ?>
+			<tr><?php echo  (method_exists($value, 'getId')) ? "<td>". $value->getId() : null ;?>
+				<?php echo  (method_exists($value, 'getName')) ? "<td>". $value->getName()  : null ;?>
+				<?php echo  (method_exists($value, 'getPrix')) ? "<td>".  $value->getPrix()  : null ;?>
+				<?php echo  (method_exists($value, 'getBrand')) ? "<td>". $value->getBrand()  : null ;?>
+				<?php echo  (method_exists($value, 'getProductorName')) ? "<td>". $value->getProductorName() :null;?>
+				<?php echo  (method_exists($value, 'getHarverstedAt')) ? "<td>". $value->getHarverstedAt()  : null;?></tr>
+				<?php } ?>
+			</table>
+			<br>
+			<br>
+			<br>
+			
 
-</body>
-</html>
+		</body>
+		</html>
